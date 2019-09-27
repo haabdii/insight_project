6d5d026b0b2e6e242a4ff820329cdc5a1044a3d7
Hamed <Hamed@Hameds-MacBook-Air.local>
2019-09-27 16:32:57 -0400

    first commit

--- app.py
+++ app.py
@@ -1,8 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import pickle
+
 app = Flask(__name__)
-@app.route('/')
-def hello():
-    return "Insight web app created by Hamed Abdi."
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+
+    output = None
+
+    if request.method == "POST":
+        # get url that the user has entered
+        print(request.form)
+        age = dict(request.form)["age"]
+        height = dict(request.form)["height"]
+        weight = dict(request.form)["weight"]
+        
+        # Load model
+        #model = pickle.load(open("model.pickle", "rb"))
+        #output = model.predict(X)
+        output = int(age) + int(height)
+
+    return render_template('index.html', output=output)
 
 if __name__ == '__main__':
     app.run()
