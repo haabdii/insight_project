f4a72d80322da50bcc6c8fe908aebacb206fc0be
Hamed <Hamed@Hameds-MacBook-Air.local>
2019-09-29 21:54:46 -0400

    first commit

--- app_2.py
+++ app_2.py
@@ -14,18 +14,11 @@ def index():
         age = dict(request.form)["age"]
         height = dict(request.form)["height"]
         weight = dict(request.form)["weight"]
-        dob = dict(request.form)["dob"]
-        position = dict(request.form)["position"]
-        start = dict(request.form)["start"]
-        #print(dict(request.form)["2016"])
-        #print(dict(request.form)["2017"])
-        #year_2018 = dict(request.form)["2018"]
         
         # Load model
         #model = pickle.load(open("model.pickle", "rb"))
         #output = model.predict(X)
-        #output = int(age) + int(height)
-        output = 0.61
+        output = int(age) + int(height)
 
     return render_template('index.html', output=output)
 
