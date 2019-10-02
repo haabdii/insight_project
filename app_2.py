d653093a7ab518b37fbc7d68409687a3dedf7bc8
Hamed <Hamed@Hameds-MacBook-Air.local>
2019-10-02 16:22:36 -0400

    first commit

--- app_2.py
+++ app_2.py
@@ -61,3 +61,26 @@ def index():
 
 if __name__ == '__main__':
     app.run()
+
+
+
+
+from flask import Flask, render_template, request
+import pickle
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    
+    output = None
+    
+    if request.method == "POST":
+        
+        output = 0.49
+    
+    return render_template('index.html', output=output)
+
+if __name__ == '__main__':
+    app.run()
+
